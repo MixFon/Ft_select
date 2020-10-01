25375f91b9f1332c1bedd942d2ab0a19adce754a
MixFon <mixfon@rambler.ru>
2020-10-01 10:00:25 +0300

    add norme

--- include/ft_select.h
+++ include/ft_select.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_select.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: widraugr <widraugr@student.21-school.      +#+  +:+       +#+        */
+/*   By: widraugr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/10/01 08:58:14 by widraugr          #+#    #+#             */
-/*   Updated: 2020/10/01 09:29:38 by widraugr         ###   ########.fr       */
+/*   Created: 2020/10/01 09:51:36 by widraugr          #+#    #+#             */
+/*   Updated: 2020/10/01 09:55:39 by widraugr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,53 +67,53 @@ typedef struct		s_select
 
 t_select			*g_sel;
 
-void	press_esc(t_select *sel);
-void	set_signals(void);
-void	working_signals(int sig);
+void				press_esc(t_select *sel);
+void				set_signals(void);
+void				working_signals(int sig);
 /*
 ** File new_elem.c
 */
-t_elem	*new_elem(char *title);
-void	to_loop_elements(t_select *sel);
-void	removing_unnecessary_elements(t_select *sel);
-void	infill_args(t_select *sel, int ac, char **av);
-void	print_elem(t_elem *elem);
+t_elem				*new_elem(char *title);
+void				to_loop_elements(t_select *sel);
+void				removing_unnecessary_elements(t_select *sel);
+void				infill_args(t_select *sel, int ac, char **av);
+void				print_elem(t_elem *elem);
 /*
 ** File check_arguments.c
 */
-void	check_arguments(t_select *sel, int ac, char **av);
-int		hash_sum(char *buf);
-void	press_esc(t_select *sel);
-void	press_right(t_select *sel);
-void	press_left(t_select *sel);
+void				check_arguments(t_select *sel, int ac, char **av);
+int					hash_sum(char *buf);
+void				press_esc(t_select *sel);
+void				press_right(t_select *sel);
+void				press_left(t_select *sel);
 /*
 ** File press_space.c
 */
-void	press_space(t_select *sel);
-void	press_up(t_select *sel);
-void	press_down(t_select *sel);
-void	press_enter(t_select *sel);
-void	calculate_max_len(t_select *sel);
+void				press_space(t_select *sel);
+void				press_up(t_select *sel);
+void				press_down(t_select *sel);
+void				press_enter(t_select *sel);
+void				calculate_max_len(t_select *sel);
 /*
 ** File press_delete.c
 */
-void	press_delete(t_select *sel);
-void	working_key(t_select *sel, int key);
-void	print_title(t_elem *elem, int max_len);
-void	print_elements(t_select *sel);
-int		calculate_colum(t_select *sel);
+void				press_delete(t_select *sel);
+void				working_key(t_select *sel, int key);
+void				print_title(t_elem *elem, int max_len);
+void				print_elements(t_select *sel);
+int					calculate_colum(t_select *sel);
 /*
 ** File print_elem_or_error_size.c
 */
-void	print_elem_or_error_size(t_select *sel);
-void	work(t_select *sel);
-void	seve_temp(t_select *sel);
-void	background_mode(void);
-void	standart_mode(void);
+void				print_elem_or_error_size(t_select *sel);
+void				work(t_select *sel);
+void				seve_temp(t_select *sel);
+void				background_mode(void);
+void				standart_mode(void);
 /*
 ** File working_signals.c
 */
-void	working_signals(int sig);
-void	set_signals(void);
-int		main(int ac, char **av);
+void				working_signals(int sig);
+void				set_signals(void);
+int					main(int ac, char **av);
 #endif
